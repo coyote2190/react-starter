51542a067b1e0638ca0962a007ae9159fde9dca9
Yannick Khalfouni <khalfouni.yannick@gmail.com>
2026-08-31 15:28:54 +0200

    💄 Add tailwind css v4

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
-import { fileURLToPath, URL } from 'node:url'
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import { fileURLToPath, URL } from "node:url";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
-})
+});
